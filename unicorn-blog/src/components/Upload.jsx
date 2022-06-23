a0da05d8e13a9bcf7b47cfd4889f675ec3dcbf67
slami <lamislimane@gmail.com>
2022-06-23 11:10:06 +0200

    Add upload page + global css

--- unicorn-blog/src/components/Upload.jsx
+++ unicorn-blog/src/components/Upload.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Upload = () => {
   return (
-    <div>Upload</div>
-  )
-}
+    <div className="Upload">
+      <form method="" action="">
+        <fieldset>
+          <legend> <h3> Créer un article </h3></legend>
+          <label>Le titre : </label>
+          <input type="text" id="title" />
+          <p>
+            <label>Le contenu :</label>
+            <textarea id="contents" cols="40" rows="4"></textarea>
+          </p>
+        </fieldset>
+        <button className="send-btn">Envoyer</button>
+      </form>
+    </div>
+  );
+};
 
-export default Upload
+export default Upload;
